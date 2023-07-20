9339f7de8128dad963ea9431a45906876aa5f908
Lacctoff <wolilacctoff@gmail.com>
2023-07-20 16:23:32 -0700

    Adding files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ * Return: no return cause we are applying a void at the start
+ * of the function declaration
+ */
+
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
