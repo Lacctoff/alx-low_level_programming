6622b0c26bb7e44c3ae526d100b819f8320bd250
Lacctoff <wolilacctoff@gmail.com>
2023-07-24 16:16:07 -0700

    Adding 3-puts files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line to the stdout.
+ * @str: input string.
+ * Return: 0.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
