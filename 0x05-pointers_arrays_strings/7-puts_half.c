17ca32dfdd7aa2789cf423ae1f65c9f07e42ceef
Lacctoff <wolilacctoff@gmail.com>
2023-07-24 17:36:06 -0700

    Adding 7-puts_half.c files

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+void puts_half(char *str)
+{
+	int i, n;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	n = (count - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
