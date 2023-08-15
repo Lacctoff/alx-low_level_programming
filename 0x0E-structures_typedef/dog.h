8ec368ccd0e32d503932c825d726fbe3d87f9666
Lacctoff <wolilacctoff@gmail.com>
2023-08-14 18:22:15 -0700

    Adding files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,13 +18,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_info - Typedef for struct dog
- */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-/*void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-*/
+
 #endif
