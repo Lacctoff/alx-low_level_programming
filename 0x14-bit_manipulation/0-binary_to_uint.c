58e7f06b4799c7b09c138adb87267e8f67f4afab
Lacctoff <wolilacctoff@gmail.com>
2023-08-31 17:07:28 -0700

    Testing code

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b == '0' || *b == '1')
+		{
+			result = result * 2 + (*b - '0');
+			b++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (result);
+}
