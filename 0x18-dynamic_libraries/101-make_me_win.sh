75bd0d7660442a62f343d3abcacec2a47010277f
Lacctoff <lasisitofunmi@gmail.com>
2023-10-16 20:07:49 -0700

    Finally won the jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprintf.so -fPIC test.c
-export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
+wget -P /tmp/ https://raw.github.com/Lacctoff/alx-low_level_programming/master/0x18-dynamic_libraries/myprintf.so
+export LD_PRELOAD=/tmp/myprintf.so
